fddf4c360779c4fd3918bff35b9efa68bf332701
TheonlyIcebear <78031685+TheonlyIcebear@users.noreply.github.com>
2022-08-14 04:11:13 -0400

    Update ChatSpam.py

--- ChatSpam.py
+++ ChatSpam.py
@@ -51,19 +51,19 @@ class Main:
 
 	def Connect(self):
 		return create_connection("wss://sio-bf.blox.land/socket.io/?EIO=3&transport=websocket", header={
-												"Accept-Encoding": "gzip, deflate, br",
-												"Accept-Language": "en-US,en;q=0.9",
-												"Cache-Control": "no-cache",
-												"Connection": "Upgrade",
-												"Host": "sio-bf.blox.land",
-												"Origin": "https://bloxflip.com",
-												"Pragma": "no-cache",
-												"Sec-WebSocket-Extensions": "permessage-deflate; client_max_window_bits",
-												"Sec-WebSocket-Key": "dTCC7XK7OBweEv1kVAUycQ==",
-												"Sec-WebSocket-Version": "13",
-												"Upgrade": "websocket",
-												"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
-										})
+					"Accept-Encoding": "gzip, deflate, br",
+					"Accept-Language": "en-US,en;q=0.9",
+					"Cache-Control": "no-cache",
+					"Connection": "Upgrade",
+					"Host": "sio-bf.blox.land",
+					"Origin": "https://bloxflip.com",
+					"Pragma": "no-cache",
+					"Sec-WebSocket-Extensions": "permessage-deflate; client_max_window_bits",
+					"Sec-WebSocket-Key": "dTCC7XK7OBweEv1kVAUycQ==",
+					"Sec-WebSocket-Version": "13",
+					"Upgrade": "websocket",
+					"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
+			})
 		
 
 
